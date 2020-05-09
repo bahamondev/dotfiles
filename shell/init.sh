10aff2900fcafbde18a0be7f79375ad244a43443
bahamondev <jose@bahamonde.dev>
2020-05-09 14:22:48 +0200

    feat: First Version with SDKMAN and custom workspace commands

---
+++ shell/init.sh
@@ -0,0 +1,13 @@
+# This is a useful file to have the same aliases/functions in bash and zsh
+ulimit -n 200000
+ulimit -u 2048
+
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+# Register custom aliases and functions
+for aliasToSource in "$DOTFILES_PATH/shell/_aliases/"*; do source "$aliasToSource"; done
+for exportToSource in "$DOTFILES_PATH/shell/_exports/"*; do source "$exportToSource"; done
+for functionToSource in "$DOTFILES_PATH/shell/_functions/"*; do source "$functionToSource"; done
+
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
