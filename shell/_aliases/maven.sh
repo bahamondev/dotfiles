8a841c4fd2cee439926a26c7796851b7c1dc13af
bahamondev <jose@bahamonde.dev>
2021-01-14 13:48:20 +0100

    fix: maven aliases now work

--- shell/_aliases/maven.sh
+++ shell/_aliases/maven.sh
@@ -1,3 +1,3 @@
-alias mvnc="maven clean compile -Dmaven.test.skip"
-alias mvnp="maven clean package -Dmaven.test.skip"
-alias mvni="maven clean install -Dmaven.test.skip"
+alias mvnc="mvn clean compile -Dmaven.test.skip"
+alias mvnp="mvn clean package -Dmaven.test.skip"
+alias mvni="mvn clean install -Dmaven.test.skip"
